e78d37bff1e7a4682b1e7e73b4df75b541d39471
pongkitkkpk <pongkitkim@gmail.com>
2023-01-12 22:08:03 +0700

    Edit code 0.1

--- ex04/src/main.rs
+++ ex04/src/main.rs
@@ -1,51 +1,36 @@
 use std::io;
 use std::io::Write;
-fn is_prime(n: i32) -> bool {
-    if n < 2 {
-        return false;
-    }
 
-    let mut i = 2;
-    while i * i <= n {
-        if n % i == 0 {
-            return false;
-        }
-        i += 1;
-    }
-
-    true
-}
 fn main() {
     let mut input = String::new();
-    print!("Enter number: ");
+    print!("Enter number : ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut input).expect("Failed ReadLine");
-    let number:i32=input.trim().parse().expect("Enter Number");
-
+    let mut number:i32=input.trim().parse().expect("Enter Number");
     print!("{} =",number);
-    let mut cal:i32 = number;
-    if is_prime(number){
-        print!("{}", number);
-        println!();
-        
-    }else{
-       
-        let mut prime = 2;
-        loop {
-            if is_prime(prime){
-               if is_prime(cal) || cal <= 2{
-                print!(" {}", cal);
-                break;
-                }else if cal % prime == 0 {
-                    cal = cal / prime;
-                    print!(" {} *", prime);
-                }else{
-                    prime = prime + 1;
-                }  
+    let mut count:i32=2;
+    'out:loop{
+        if number % count ==0{
+            'inner:loop{
+                if number % count ==0{
+                    number /= count;
+                    print!("{}",count);
+                    if number >1{
+                        print!("*");
+                    }else{
+                        break 'inner;
+                    }
+                }
+                else{
+                    break 'inner;
+                }
             }
-           else{
-                prime = prime + 1;
-            }
+        }
+        count += 1;
+        if number<=1{
+            break 'out;
         }
     }
 }
+
+
